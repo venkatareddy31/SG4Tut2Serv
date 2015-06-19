d275f27bf59b5eda55c0bec52670043e3ca11823
Kothuri <ukzr6@kc-sce463lab-14.kc.umkc.edu>
2015-06-19 17:51:51 -0500

    Initial Commit

---
+++ SG4Tut4Serv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SG4Tut4Serv
+//
+//  Created by Kothuri, Umamaheswara Rao (UMKC-Student) on 6/19/15.
+//  Copyright (c) 2015 Kothuri, Umamaheswara Rao (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SG4Tut4Serv/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SG4Tut4Serv
+//
+//  Created by Kothuri, Umamaheswara Rao (UMKC-Student) on 6/19/15.
+//  Copyright (c) 2015 Kothuri, Umamaheswara Rao (UMKC-Student). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
